9812ceadf58f707d863ed452c33406fb1e3ff0d5
Sai On Action <gautamsairash@gmail.com>
2025-05-24 21:42:01 +0545

    adedd type for more readablity

--- src/utility/helper.ts
+++ src/utility/helper.ts
@@ -3,26 +3,29 @@ import { type Response } from 'express';
 import { BAD_REQUEST, SUCCESS_REQUEST } from './httpStatusCodes';
 
 
-export function generateRandomString(length:number):string {
-    return randomBytes(length/2 < 1? 1: length/2).toString('hex')
+type success_names = "custom_success" | "redirect" | "sesson_token";
+type error_names = "custom_error" | "redirect" | "zod_error" | "server_error";
+
+export function generateRandomString(length: number): string {
+    return randomBytes(length / 2 < 1 ? 1 : length / 2).toString('hex')
 }
 
 
 
-export function success_json(c: Response<any, Record<string, any>>, message: string,data?: any, name?: string): Response<any, Record<string, any>> {
+export function success_json(c: Response<any, Record<string, any>>, message: string, data?: any, name?: success_names): Response<any, Record<string, any>> {
     return c.status(SUCCESS_REQUEST).json({
         data,
         message,
-        name: name??"custom_success",
+        name: name ?? "custom_success",
         success: true,
     })
 }
 
-export function error_json(c: Response<any, Record<string, any>>, message: string,data?: any, name?: string): Response<any, Record<string, any>> {
+export function error_json(c: Response<any, Record<string, any>>, message: string, data?: any, name?: error_names): Response<any, Record<string, any>> {
     return c.status(BAD_REQUEST).json({
         data,
         message,
-        name: name??"custom_error",
+        name: name ?? "custom_error",
         success: false,
     })
 }
